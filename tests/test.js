8c7c42e6e25b552b1213551094d3d2f16a5dec2e
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2022-01-24 17:18:30 +0300

    Better code

--- tests/test.js
+++ tests/test.js
@@ -1,16 +1,13 @@
-const ConsoleGame = require("../index.js")
+const ConsoleGame = require("../index.js");
 
 const game = new ConsoleGame(16, 8);
 
-game.render()
+game.render();
 
-for (const [x, y] of [[2, 3], [3, 4], [4, 5], [5, 6]]) {
-    game.set(x, y)
+const write = [[2, 3], [3, 4], [4, 5], [5, 6]];
 
-}
+for (const [x, y] of write)
+    game.set(x, y);
 
-for (const [x, y] of [[2, 3], [3, 4], [4, 5], [5, 6]]) {
-    game.set(x + 2 + 4, y)
-}
-
-
+for (const [x, y] of write)
+    game.set(x + 6, y);
